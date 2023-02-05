82243fc21c436257fd4b95f0da9d0b1537d54ca4
Aleksander Dreszler <olek.d93@gmail.com>
2023-02-05 20:20:49 +0100

    - zmieniono nazwę zmiennej drinkName w klasie Drink na name - poprawiono checkstyle w main

--- src/main/java/pl/javastart/task/Drink.java
+++ src/main/java/pl/javastart/task/Drink.java
@@ -1,7 +1,7 @@
 package pl.javastart.task;
 
 public class Drink {
-    String drinkName;
+    String name;
     double price;
     boolean isAlcoholic;
     Ingredient ingredient1 = new Ingredient();
